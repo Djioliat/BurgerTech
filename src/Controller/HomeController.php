69be7da4ccb1dcec7b1ca48206ad3704d9b10dd4
djioliat <alverbreugh@gmail.com>
2022-04-14 14:49:03 +0200

    Procfal

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
         'episode' => $episodeRepository->findBy([],
         ['id' => 'DESC'])
-        ]); 
+        ]);
     }
     
     #[Route('/ajout', name: 'app_ajout')]

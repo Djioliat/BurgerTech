002acd3a4f737e3e77fd70ac4bb2bf79fc2d2eb2
Djioliat <alverbreugh@gmail.com>
2022-11-07 22:43:18 +0100

    correction erreur mig.

--- migrations/Version20221106205719.php
+++ migrations/Version20221106205719.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221107110717 extends AbstractMigration
+final class Version20221106205719 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20221107110717 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comment ADD users_id INT NOT NULL');
+        $this->addSql('ALTER TABLE comment ADD users_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C67B3B43D FOREIGN KEY (users_id) REFERENCES users (id)');
         $this->addSql('CREATE INDEX IDX_9474526C67B3B43D ON comment (users_id)');
     }

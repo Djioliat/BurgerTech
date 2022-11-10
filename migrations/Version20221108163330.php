46acb96e3ead5bc649d2927dffecb1bca15911d1
Djioliat <alverbreugh@gmail.com>
2022-11-10 08:56:08 +0100

    heroku

---
+++ migrations/Version20221108163330.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221108163330 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE episode_articles');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE episode_articles (episode_id INT NOT NULL, articles_id INT NOT NULL, INDEX IDX_765B181A1EBAF6CC (articles_id), INDEX IDX_765B181A362B62A0 (episode_id), PRIMARY KEY(episode_id, articles_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE episode_articles ADD CONSTRAINT FK_765B181A1EBAF6CC FOREIGN KEY (articles_id) REFERENCES articles (id) ON UPDATE NO ACTION ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE episode_articles ADD CONSTRAINT FK_765B181A362B62A0 FOREIGN KEY (episode_id) REFERENCES episode (id) ON UPDATE NO ACTION ON DELETE CASCADE');
+    }
+}

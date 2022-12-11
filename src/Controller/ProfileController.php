1bcf71f745e6416f7024167bf9825e4e68f3a7fb
Djioliat <alverbreugh@gmail.com>
2022-12-11 18:13:19 +0100

    ajout mon compte nav-bar

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -7,14 +7,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/profil', name: 'profile_')]
+
 class ProfileController extends AbstractController
 {
-    #[Route('/{pseudo}', name: 'index')]
-    public function index(Users $user): Response
+    #[Route('/profil', name: 'profil_index')]
+    public function index(): Response
     {
-        return $this->render('profile/index.html.twig', [
-            'user' => $user
-        ]);
+        return $this->render('profil/index.html.twig');
     }
 }

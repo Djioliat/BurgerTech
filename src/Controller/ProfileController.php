b94fff3207ea68f8a8c25e3320275785c828d952
Djioliat <alverbreugh@gmail.com>
2022-12-12 22:55:49 +0100

    page-profil

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Users;
+use App\Repository\UsersRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class ProfileController extends AbstractController
@@ -14,5 +17,33 @@ class ProfileController extends AbstractController
     {
         return $this->render('profil/index.html.twig');
 
+    }
+
+    /* #[Route('/profil/supprimer', name: 'profil_delete')]
+    public function delete(Users $user,EntityManagerInterface $manager): Response
+    {
+        $user = $this->getUser($user);
+        $manager->remove($this->getUser($user));
+        $manager->flush();
+
+        $this->addFlash('success', "L'utilisateur à bien été supprimé !");
+
+        return $this->redirectToRoute('home/index.html.twig', [
+            'user' => $user
+        ]); 
+          
+    } */
+    #[Route('/profil/supprimer/{id}', name: 'profil_delete')]
+    public function delete(int $id, EntityManagerInterface $manager): Response
+    {
+        $user = $manager->getRepository(Users::class)->find($id);
+        $manager->remove($user);
+        $manager->flush();
+
+        $this->addFlash('success', "L'utilisateur à bien été supprimé !");
+
+        return $this->redirectToRoute('home/index.html.twig', [
+            'user' => $user 
+        ]); 
     }
 }
